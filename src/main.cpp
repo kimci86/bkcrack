d566075f97e680193c1f468daaef7891fb430272
kimci86 <kimci86@hotmail.fr>
2024-01-22 22:02:20 +0100

    Define version date manually for reproducibility

--- src/main.cpp
+++ src/main.cpp
@@ -107,7 +107,7 @@ try
     VirtualTerminalSupport vtSupport;
 
     // version information
-    std::cout << "bkcrack " BKCRACK_VERSION " - " BKCRACK_COMPILATION_DATE << std::endl;
+    std::cout << "bkcrack " BKCRACK_VERSION " - " BKCRACK_VERSION_DATE << std::endl;
 
     const Arguments args(argc, argv);
     if(args.help)
